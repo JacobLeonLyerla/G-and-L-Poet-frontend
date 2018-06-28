31cecede41b3fe317d77e83105f2b9a5e5d8ed43
Jacob Leon Lyerla <umasour@yahoo.com>
2018-06-27 23:02:26 -0700

    set up a new react app
    set up animations  using animate.css and fontawesome. set up less. and started running some test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
-
-import "./App.css";
-
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './css/index.css'
 
 class App extends Component {
-
-
-  
   render() {
     return (
-      <div className="">
-        <i className="fas fa-bowling-ball animated infinite rubberBand" />
+      <div className="App">
+   <i class="fab fa-twitter animated zoomInDown"></i>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    //wrapping App component with the Router
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById("root")
-  );
-  
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
